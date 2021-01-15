5c120b81b4392a10fd56c31c218b264c349b6ee9
Abdur Rahim <rahimbd623@gmail.com>
2021-01-15 15:43:34 +0600

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Iwhe msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import Iwhe from "@/components/Iwhe.vue";
+import Iwhe from "../components/Iwhe.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    Iwhe,
+  },
+
+  Iwhe,
+};
 </script>
